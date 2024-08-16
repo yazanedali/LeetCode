c9a676f42b2a90418a26bed136269c1efe7468c6
yazanedali <160069406+yazanedali@users.noreply.github.com>
2024-08-16 14:36:34 +0300

    Time: 0 ms (100.00%) | Memory: 44.2 MB (62.37%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -16,8 +16,6 @@
 class Solution {
    private int res;
 public int maxPathSum(TreeNode root) {
-        if (root == null)
-            return 0;
         res = root.val;
         dfs(root);
         return res;
@@ -29,8 +27,6 @@ public int maxPathSum(TreeNode root) {
         int leftmax = Math.max(dfs(root.left),0); //to avoid -ve value
         int rightnax = Math.max(dfs(root.right),0); // to avoid -ve value
         res = Math.max(res , root.val + leftmax + rightnax);
-        System.out.println(root.val + Math.max(leftmax, rightnax));
-        //return the maximum gain that can be achieved by including the current node
         return root.val + Math.max(leftmax, rightnax);
     }
 }
