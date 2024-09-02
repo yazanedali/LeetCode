c761f5812f4b648dde783b409063e116720cc39e
yazanedali <160069406+yazanedali@users.noreply.github.com>
2024-09-02 19:09:49 +0300

    Time: 0 ms (100%), Space: 41.6 MB (77.07%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,20 +1,21 @@
 class Solution {
 
-        public int numDecodings(String s) {
+    public int numDecodings(String s) {
         if (s == null || s.length() == 0)
             return 0;
 
         int []dp = new int [s.length() +1];
-        dp[s.length()] = 1;
+        dp[s.length()] = 1; //end of the string, there is only one way to decode it(not the last number).
 
         for (int i= s.length() - 1; i >= 0; i--) {
             if (s.charAt(i) == '0')
-                dp[i] = 0;
+                dp[i] = 0;  // This position cannot be the beginning of a correct decoding
             else {
                 dp[i] = dp[i + 1];
 
                 if (i + 1 < s.length() && (s.charAt(i) == '1' ||
                         (s.charAt(i) == '2' && s.charAt(i + 1) >= '0' && s.charAt(i + 1) <= '6'))) 
+                    //If we can use the current letter with the next letter to form a number between "10" and "26"
                     dp[i] += dp[i + 2];
             }
         }
